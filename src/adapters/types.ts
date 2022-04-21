5b0bc013d0754cdd396e364b617cfa7ed0df86ef
Sitch <michael.sitchenko@gmail.com>
2022-04-21 13:50:24 -0400

    Changes via pushit

--- src/adapters/types.ts
+++ src/adapters/types.ts
@@ -59,19 +59,42 @@ export interface TableStatistics {
   statistics?: ColumnStatistics[]
 }
 
-export type ColumnStatistics =
-  | NumericalColumnStatistics
-  | TextColumnStatistics
-  | BooleanColumnStatistics
-
-interface DefaultColumnStatistics<T> {
-  is_null_present: boolean
-  cardinality: number
-  categories: CategoryStatistics<T>[]
+export interface CategoryStatistics<T> {
+  label: string
+  value: T | null
+  frequency: number
+  relative_frequency: number
 }
 
-type TextColumnStatistics = DefaultColumnStatistics<string>
-type BooleanColumnStatistics = DefaultColumnStatistics<boolean>
+export enum InferredPTypeEnum {
+  boolean = 'boolean',
+  categorical = 'categorical',
+  date = 'date',
+  float = 'float',
+  integer = 'integer',
+  string = 'string',
+}
+
+export type InferredPType =
+  | InferredPTypeEnum.boolean
+  | InferredPTypeEnum.categorical
+  | InferredPTypeEnum.date
+  | InferredPTypeEnum.float
+  | InferredPTypeEnum.integer
+  | InferredPTypeEnum.string
+
+interface PType<T> {
+  inferred_ptype: InferredPType
+  anomalous_values: InferredPType[]
+  missing_values: InferredPType[]
+  normal_values: T[]
+}
+interface DefaultColumnStatistics<T> {
+  cardinality: number
+  is_null_present: boolean
+  categories: CategoryStatistics<T>[]
+  ptype: PType<T>
+}
 
 export interface NumericalColumnStatistics extends DefaultColumnStatistics<number> {
   mean: number
@@ -88,12 +111,25 @@ export interface NumericalColumnStatistics extends DefaultColumnStatistics<numbe
   mode: number
 }
 
-export interface CategoryStatistics<T> {
-  label: string
-  value: T | null
-  frequency: number
-  relative_frequency: number
+export interface TextColumnStatistics extends DefaultColumnStatistics<string> {
+  minimum_length: number
+  maximum_length: number
+  range: number
+  standard_deviation: number
+  variance: number
+  q1: number
+  q3: number
+  iqr: number
+  skewness: number
+  mode: number
 }
+
+type BooleanColumnStatistics = DefaultColumnStatistics<boolean>
+
+export type ColumnStatistics =
+  | NumericalColumnStatistics
+  | TextColumnStatistics
+  | BooleanColumnStatistics
 
 //------------------------------------------------------------------------------
 
