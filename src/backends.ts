bad0f1588ac480f1e3c04fc9ecc0846e03c99d66
Sitch <michael.sitchenko@gmail.com>
2022-04-25 13:58:32 -0400

    Changes via pushit

--- src/backends.ts
+++ src/backends.ts
@@ -1,13 +1,3 @@
-import { render_algebraic_julia } from './backends/algebraic-julia'
-import { render_hydra } from './backends/hydra'
-import { render_json } from './backends/json'
-import { render_julia } from './backends/julia'
-import { render_julia_genie } from './backends/julia-genie'
-import { render_julia_octo } from './backends/julia-octo'
-import { render_typedb } from './backends/typedb'
-import { render_typedb_loader_config } from './backends/typedb-loader-config'
-import { render_typescript } from './backends/typescript'
-
 //------------------------------------------------------------------------------
 
 export enum DataSourceEnum {
@@ -34,36 +24,3 @@ export enum BackendEnum {
 export type BackendName = keyof typeof BackendEnum
 
 //------------------------------------------------------------------------------
-
-export const apply_backend = (backend: BackendName) => {
-  if (backend === 'typescript') {
-    return render_typescript
-  }
-  if (backend === 'json') {
-    return render_json
-  }
-  if (backend === 'typedb') {
-    return render_typedb
-  }
-  if (backend === 'julia') {
-    return render_julia
-  }
-  if (backend === 'algebraic_julia') {
-    return render_algebraic_julia
-  }
-  if (backend === 'julia_genie') {
-    return render_julia_genie
-  }
-  if (backend === 'julia_octo') {
-    return render_julia_octo
-  }
-  if (backend === 'hydra') {
-    return render_hydra
-  }
-  if (backend === 'typedb_loader_config') {
-    return render_typedb_loader_config
-  }
-  throw `Invalid backend: ${backend} must be one of: ${Object.values(BackendEnum).join(
-    ',',
-  )}`
-}
--- src/compiler.ts
+++ src/compiler.ts
@@ -14,7 +14,16 @@ import type {
   TableCommentDefinition,
   TableDefinition,
 } from './adapters/types'
-import { apply_backend, BackendName, DataSource } from './backends'
+import { BackendEnum, BackendName, DataSource } from './backends'
+import { render_algebraic_julia } from './backends/algebraic-julia'
+import { render_hydra } from './backends/hydra'
+import { render_json } from './backends/json'
+import { render_julia } from './backends/julia'
+import { render_julia_genie } from './backends/julia-genie'
+import { render_julia_octo } from './backends/julia-octo'
+import { render_typedb } from './backends/typedb'
+import { render_typedb_loader_config } from './backends/typedb-loader-config'
+import { render_typescript } from './backends/typescript'
 import { Config, get_user_imports, UserImport } from './config'
 import { merge_table_meta } from './tables'
 import { validate_coreferences, validate_enums, validate_tables } from './validators'
@@ -107,6 +116,39 @@ const compile = async (
     nodes: [],
     edges: [],
   }
+}
+
+const apply_backend = (backend: BackendName) => {
+  if (backend === 'typescript') {
+    return render_typescript
+  }
+  if (backend === 'json') {
+    return render_json
+  }
+  if (backend === 'typedb') {
+    return render_typedb
+  }
+  if (backend === 'julia') {
+    return render_julia
+  }
+  if (backend === 'algebraic_julia') {
+    return render_algebraic_julia
+  }
+  if (backend === 'julia_genie') {
+    return render_julia_genie
+  }
+  if (backend === 'julia_octo') {
+    return render_julia_octo
+  }
+  if (backend === 'hydra') {
+    return render_hydra
+  }
+  if (backend === 'typedb_loader_config') {
+    return render_typedb_loader_config
+  }
+  throw `Invalid backend: ${backend} must be one of: ${Object.values(BackendEnum).join(
+    ',',
+  )}`
 }
 
 export async function render(context: BuildContext, backend: BackendName) {
