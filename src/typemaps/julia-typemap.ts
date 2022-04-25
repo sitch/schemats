59418adafc57914917cb5073d58e649c876d8f93
Sitch <michael.sitchenko@gmail.com>
2022-04-25 13:17:02 -0400

    Changes via pushit

--- src/typemaps/julia-typemap.ts
+++ src/typemaps/julia-typemap.ts
@@ -1,6 +1,5 @@
 import type { PropertyDefinition } from '../adapters/types'
-import type { BuildContext } from '../compiler'
-import { DataSource, DataSourceEnum } from '../compiler'
+import { BuildContext, DataSource, DataSourceEnum } from '../compiler'
 import type { UDTTypeMap } from '../coreference'
 
 export type JuliaType = string
